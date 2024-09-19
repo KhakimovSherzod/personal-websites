f7e820e2b5dd1ea8306e8c17234db7efcc0dbf83
Sherzod Khakimov <sherzodh812@gmail.com>
2024-09-19 23:26:30 +0800

    personal project fadul

---
+++ personal portfolio/personal portfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Helvetica_neue: ["Helvetica-neue", "sans-serif"],
+      Helvetica_neue_light: ["Helvetica-neue-light", "sans-serif"],
+      Almarai_Light: ["Almarai-Light", "sans-serif"],
+      Almarai_regular: ["Almarai-regular", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
